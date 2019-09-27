6d48b17a108b222f5a29b0e4d37ac9e9eca0d8c7
Taof <taof@Taofs-MacBook-Pro.local>
2019-09-27 11:15:47 +0700

    main.dart: in ra màn hình dòng chữ helloworld, fontsize 24

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'First Project'),
     );
   }
 }
@@ -92,20 +92,12 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+              'HelloWorld',
+              style: TextStyle(fontSize: 24)
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
