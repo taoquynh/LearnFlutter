a0de858f58439ecd6a961425ddc0dcf7de0829da
Taof <taof@Taofs-MacBook-Pro.local>
2019-10-01 06:12:28 +0700

    project sưu tầm: animated, position, scale, opcity

--- flutter_implicit_animations/ios/Flutter/flutter_export_environment.sh
+++ flutter_implicit_animations/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/taof/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/taof/Taof/Flutter/flutter_implicit_animations"
-export "FLUTTER_TARGET=/Users/taof/Taof/Flutter/flutter_implicit_animations/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/taof/LearnFlutter/flutter_implicit_animations"
+export "FLUTTER_TARGET=/Users/taof/LearnFlutter/flutter_implicit_animations/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/taof/flutter/bin/cache/artifacts/engine/ios"
