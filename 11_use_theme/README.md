2e0b249f9354aa174a6fa532f52ddf406b9b79ee
Taof <taof@Taofs-MacBook-Pro.local>
2019-10-04 10:37:30 +0700

    project demo custom theme

---
+++ 11_use_theme/README.md
@@ -0,0 +1,7 @@
+# use_theme
+
+- Custom theme 
+- Background Image
+- Box Fit
+
+<img src = "../img/use_theme.png" width="300">
---
+++ 11_use_theme/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp( 
+      title: 'Use Theme',
+      home: MyHome(),
+      theme: ThemeData( 
+        primaryColor: Colors.red,
+        accentColor: Colors.teal,
+        // backgroundColor: Colors.black12
+      ),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Use Theme'),
+        backgroundColor: Colors.teal,), // màu appbar 
+      body: Container( 
+        // một cách khác để đặt background (không sử dụng đồng thời với color)
+        decoration: BoxDecoration(
+          color: Colors.black87,
+          // background image from url
+          image: DecorationImage(
+            image: NetworkImage('https://img.thuthuatphanmem.vn/uploads/2018/09/28/hinh-nen-iphone-dep-nhat_030009517.jpg',),
+            fit: BoxFit.fill) // fit image
+          ),
+        // color: Colors.teal,
+            padding: EdgeInsets.only(bottom: 500),
+        child: Center( 
+          child: Container( 
+            // sử dụng theme accent color như background color cho widget con này
+            color: Theme.of(context).accentColor,
+            child: Text( 
+              'Hello World!',
+              // style: Theme.of(context).textTheme.title, // đặt kiểu văn bản theo theme
+              style: TextStyle( 
+                color: Theme.of(context).textTheme.title.color,
+                fontSize: 32
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: Theme( 
+        data: Theme.of(context).copyWith(
+          colorScheme: Theme.of(context).colorScheme.copyWith(secondary: Colors.red),
+        ),
+        child: FloatingActionButton(
+          child: Icon(Icons.adb, color: Colors.white,),
+          onPressed: null,
+        ),
+      ),
+    );
+  }
+}
