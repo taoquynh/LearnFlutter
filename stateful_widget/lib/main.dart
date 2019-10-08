b015a127f7cdd4883062e508cf0f206c6086deef
Taof <taof@Taofs-MacBook-Pro.local>
2019-10-08 09:23:03 +0700

    hoàn thành ví dụ stateful_widget

--- stateful_widget/lib/main.dart
+++ stateful_widget/lib/main.dart
@@ -14,17 +14,34 @@ class MyButton extends StatefulWidget{
 }
 
 class MyButtonState extends State<MyButton>{
-  int counter = 0;
-  List<String> strings = ['Tào', 'Thuý', 'Quỳnh'];
-  String displayedString = '';
+  int counter = 0; // tạo biến đếm
+  List<String> strings = ['Trăm', 'Năm', 'Trong', 'Cõi', 'Người', 'Ta']; // mảng dữ liệu
+  String displayedString = ''; 
 
+  void onPressOfButton(){
+    setState(() {
+      displayedString = strings[counter];
+      counter = counter < strings.length - 1 ? counter + 1 : 0;
+    });
+  }
   @override 
   Widget build(BuildContext context){
     return Scaffold( 
       appBar: AppBar(title: Text('Statefull Widget'),),
       body: Container( 
         child: Center( 
-          
+          child: Column( 
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: <Widget>[
+              Text(displayedString, style: TextStyle(fontSize: 40.0),),
+              Padding(padding: EdgeInsets.all(10.0),),
+              RaisedButton( 
+                child: Text('Press me', style: TextStyle(color: Colors.white),),
+                onPressed: onPressOfButton,
+                color: Colors.red,
+              )
+            ],
+          ),
         ),
       ),
     );
