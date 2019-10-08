d96cc7ebc6d27fca5faf1ddda47dcd005d94c9e0
Taof <taof@Taofs-MacBook-Pro.local>
2019-10-08 16:07:40 +0700

    alert dialog default

---
+++ e_08_alert_dialog/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  AlertDialog dialog = AlertDialog(
+    title: Text('Show Dialog'),
+    actions: <Widget>[
+      new FlatButton(
+        child: new Text('Close'),
+        onPressed: (){
+          print('Cancel');
+        },
+      ),
+    ],
+    content: Text('This is a dialog'),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Show Alert Dialog'),
+      ),
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            child: Text('Hit to alert'),
+            onPressed: () {
+              showDialog(
+                  context: context, builder: (BuildContext context) => dialog);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
