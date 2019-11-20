d4d094726d920338c3fc8f3261f25d12cabcabeb
Taof <taof@Taofs-MacBook-Pro.local>
2019-11-20 17:48:52 +0700

    git push origin master

---
+++ 00_Dart/BaiTap/bt_class.dart
@@ -0,0 +1,37 @@
+/*
+Xây dựng lớp Candidate (Thí sinh) gồm các thuộc tính: mã, tên, điểm thi Toán, Văn, Anh và các phương thức cần thiết.
+Xây dựng lớp TestCandidate để kiểm tra lớp trên:
+- Nhập vào n thí sinh (n do người dùng nhập)
+- In ra thông tin về các thí sinh có tổng điểm lớn hơn 15
+*/
+
+class Candidate {
+  int code;
+  String name;
+  double mathPoint;
+  double literaturePoint;
+  double englishPoint;
+
+  Candidate(int code, String name, {double mathPoint = 0.0, literaturePoint = 0.0, englishPoint = 0.0}){
+    this.code = code;
+    this.name = name;
+    this.mathPoint = mathPoint;
+    this.literaturePoint = literaturePoint;
+    this.englishPoint = englishPoint;
+  }
+
+  double tongDiem() => (mathPoint + literaturePoint + englishPoint);
+}
+
+void main(){
+  var candidate1 = Candidate(1, 'A', mathPoint: 8.0, literaturePoint: 5.0, englishPoint: 4.0);
+  var candidate2 = Candidate(2, 'B', mathPoint: 8.0, literaturePoint: 5.0);
+
+  List<Candidate> candidates = new List();
+  candidates.addAll([candidate1, candidate2]);
+  candidates.forEach( (item){
+    if (item.tongDiem() > 15.0){
+      print(item.name);
+    }
+  });
+}
---
+++ 00_Dart/ham_an_danh.dart
@@ -0,0 +1,7 @@
+void main(){
+
+  f1(int a, b (var printmessage)){
+    print('printmessage + $a + $b');
+  }
+
+}
